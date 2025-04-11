1995b28b6832d067efea852b745931c50bf7f2e0
Rachitha Kamath <rachitha.23cs109@sode-edu.in>
2025-04-11 19:10:46 +0530

    A4.asm

--- A4.asm
+++ A4.asm
@@ -1,37 +1,31 @@
-	AREA PrimeCheck, CODE, READONLY		
-START
-    MOV R0, #29 
-    MOV R1, #1 
-    
-    CMP R0, #2 
-    BLT NOT_PRIME 
-    BEQ DONE 
-    
-    MOV R2, #2 
-    
-CHECK_LOOP
-    CMP R2, R0 
-    BGE DONE
-    
-    MOV R3, R0 
-    MOV R4, #0 
-    
-SUB_LOOP
-    CMP R3, R2 
-    BLT CHECK_NEXT 
-    SUB R3, R3, R2 
-    ADD R4, R4, #1 
-    B SUB_LOOP
-    
-CHECK_NEXT
-    CMP R3, #0 
-    BEQ NOT_PRIME 
-    ADD R2, R2, #1 
-    B CHECK_LOOP
-    
-NOT_PRIME
-    MOV R1, #0 
-    
-DONE
-    B DONE 
-    END
+	AREA pgr4,CODE,READONLY
+ENTRY
+
+	MOV r0, #29
+	MOV r1, #2
+
+loop
+    MUL r2,r1,r1
+	CMP r2, r0
+	BHI  prime
+	
+	MOV r3,r0
+check_mod
+	SUBS r3,r3,r1
+	BPL check_mod
+	
+	CMP r3,#0
+	BEQ not_prime
+	
+	ADD r1,r1,#1
+	B loop
+	
+prime
+	B stop
+	
+not_prime
+
+	B stop
+stop
+	B stop
+	END 
