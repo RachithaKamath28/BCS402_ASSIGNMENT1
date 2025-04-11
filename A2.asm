c8d231b4308daf526106ecb3c04c1a76a20736ad
Rachitha Kamath <rachitha.23cs109@sode-edu.in>
2025-04-11 19:09:42 +0530

    A2.asm

--- A2.asm
+++ A2.asm
@@ -1,16 +1,13 @@
-	AREA Fibonacci, CODE, READONLY
+	AREA pgr2,CODE,READONLY
 ENTRY
-	MOV R0, #0 
-	MOV R1, #1 
-	MOV R2, #10 
-	MOV R3, #0 
-LOOP
-	STR R0, [R3] 
-	ADD R3, R3, #4 
-	MOV R4, R0 
-	ADD R0, R0, R1 
-	MOV R1, R4 
-	SUBS R2, R2, #1 
-	BNE LOOP 
-HERE B HERE 
-	END
+	MOV r0, #0
+    MOV r1, #1
+    MOV r2, #10
+loop
+    ADD r3, r0, r1
+    MOV r0, r1
+    MOV r1, r3
+    SUBS r2, r2, #1
+    BNE loop
+
+    END
